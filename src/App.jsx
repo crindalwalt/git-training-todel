6d276e54f3bed8d3d4336bb70b50bac8287900e7
M.Saim siddiqui <saimsaddiqui3@gmail.com>
2025-06-25 05:00:35 -0700

    heklo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./Sidebar";
 import Dashboard from "./components/Dashboard";
 import "./index.css";
 
