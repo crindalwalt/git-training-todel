65750f33ee6f8e46027ee253dbe02086c7380d8f
Mohsan Ali Zafar <mohsanalimohsan649@gmail.com>
2025-06-25 16:37:02 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import AboutMohsan from './AboutMohsan'
+import { NavLink } from 'react-router-dom'
 
 function App() {
   return (
     <>
+    <NavLink to='/aboutMohsan'> Go to Mohsan's Profile</NavLink>
+    
     <Routes>
       <Route path='/aboutMohsan'element={<AboutMohsan/>}/>
       
